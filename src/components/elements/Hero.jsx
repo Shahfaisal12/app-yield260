37e9ded4ba522a7efaf3dca8a010cbb5d7fffd88
shahfaisal12 <faisalmkd02@gmail.com>
2022-12-25 22:57:38 +0500

    'add'

--- src/components/elements/Hero.jsx
+++ src/components/elements/Hero.jsx
@@ -17,12 +17,12 @@ const Hero = () => {
       <div className="Hero-section pt-5">
         <div className="container">
           <div className="row">
-            <div className="h1 text-white text-start card-div">Y360 Dashboard</div>
+            <div className="h1 text-white text-start card-data">Y360 Dashboard</div>
           </div>
           <div className="container">
             <div className="row mt-5">
               <div className="col-md-4">
-                <div className="carde">
+                <div className="card-bg">
                   <div className="card-body">
                     <p className="">Y360 Price</p>
                     <h1 className="">$0.00</h1>
@@ -30,7 +30,7 @@ const Hero = () => {
                 </div>
               </div>
               <div className="col-md-4">
-                <div className="carde">
+                <div className="card-bg">
                   <div className="card-body">
                     <p className="">Market Cap</p>
                     <h1 className="">$336,192.07</h1>
@@ -38,7 +38,7 @@ const Hero = () => {
                 </div>
               </div>
               <div className="col-md-4">
-                <div className="carde">
+                <div className="card-bg">
                   <div className="card-body">
                     <p className="">Circulating Supply</p>
                     <h1 className="">360,000</h1>
@@ -47,7 +47,7 @@ const Hero = () => {
                 </div>
               </div>
               <div className="col-md-4">
-                <div className="carde">
+                <div className="card-bg">
                   <div className="card-body">
                     <p className="">Backed Liquidity</p>
                     <h1 className="">100%</h1>
@@ -55,7 +55,7 @@ const Hero = () => {
                 </div>
               </div>
               <div className="col-md-4">
-                <div className="carde">
+                <div className="card-bg">
                   <div className="card-body">
                     <p className="">Next Rebase</p>
                     <h1 className="">10:49</h1>
@@ -63,7 +63,7 @@ const Hero = () => {
                 </div>
               </div>
               <div className="col-md-4">
-                <div className="carde">
+                <div className="card-bg">
                   <div className="card-body">
                     <p className="">Max Supply</p>
                     <h1 className="">360,000,000</h1>
@@ -74,25 +74,25 @@ const Hero = () => {
             </div>
             <div className="row mt-5">
               <div className="col-md-6">
-                <div className="card-div text-center">
+                <div className="card-data text-center">
                   <p>Y360 Insurance Fund Value</p>
                   <h1>$0.00</h1>
                 </div>
               </div>
               <div className="col-md-6">
-                <div className="card-div text-center">
+                <div className="card-data text-center">
                   <p>Market Value of Treasury Asset</p>
                   <h1>$0.00</h1>
                 </div>
               </div>
               <div className="col-md-6">
-                <div className="card-div text-center">
+                <div className="card-data text-center">
                   <p>BNB Liquidity Value</p>
                   <h1>0.00 BNB</h1>
                 </div>
               </div>
               <div className="col-md-6">
-                <div className="card-div text-center">
+                <div className="card-data text-center">
                   <p>Pool Value</p>
                   <h1>$0.00</h1>
                 </div>
@@ -100,19 +100,19 @@ const Hero = () => {
             </div>
             <div className="row mt-5">
               <div className="col-md-4">
-                <div className="card-div text-center">
+                <div className="card-data text-center">
                   <p># Value of FirePit [Y360]</p>
                   <h1>0.00 Y360</h1>
                 </div>
               </div>
               <div className="col-md-4">
-                <div className="card-div text-center">
+                <div className="card-data text-center">
                   <p>$ Value of FirePit</p>
                   <h1>$0.00</h1>
                 </div>
               </div>
               <div className="col-md-4">
-                <div className="card-div text-center">
+                <div className="card-data text-center">
                   <p>% FirePit : Supply</p>
                   <h1>0.00%</h1>
                 </div>
